fb89c6bfe04271810b0f7113ac2e49deb724c4d7
Aleksey Osipov <aosipov@tradingview.com>
2020-07-12 20:26:15 +0300

    download udf

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,12 @@ time=$(date)
 echo "::set-output name=time::$time"
 env
 ls -lR
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_UDF}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_UDF}
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION_UDF}
+UDF_PATH=/usr/local/bin/udf
+aws s3 cp ${UDF_RELEASE_BUCKET}/udf_${UDF_RELEASE} ${UDF_PATH} --no-progress && chmod +x ${UDF_PATH}
+udf version
 # TODO: get the list of changed/added files
 # TODO: for each file get the range of changed bars
 # TODO: for each file and range generate the patch
