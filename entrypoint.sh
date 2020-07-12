e83ede23ed8654c31ee528b019e686acbe8a5f88
Aleksey Osipov <aosipov@tradingview.com>
2020-07-12 18:47:56 +0300

    env in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,11 @@ echo $(gh --version)
 echo $(aws --version)
 time=$(date)
 echo "::set-output name=time::$time"
+env
+
+# TODO: get the list of changed/added files
+# TODO: for each file get the range of changed bars
+# TODO: for each file and range generate the patch
+# TODO: for each file and range upload patch to S3 bucket
+# TODO: for each file and range create a task definition
+# TODO: send created tasks to SQS
