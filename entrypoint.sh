34cee7f140bfcd1f87e5d34e55f1332f9f12d0ff
Aleksey Osipov <aosipov@tradingview.com>
2020-07-12 20:45:08 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
 echo $(git version)
 echo $(gh --version)
 echo $(aws --version)
@@ -8,13 +7,20 @@ time=$(date)
 echo "::set-output name=time::$time"
 env
 ls -lR
+
+# download udf utility
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_UDF}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_UDF}
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION_UDF}
 UDF_PATH=/usr/local/bin/udf
 aws s3 cp ${UDF_RELEASE_BUCKET}/udf_r${UDF_RELEASE} ${UDF_PATH} --no-progress && chmod +x ${UDF_PATH}
-udf version
-# TODO: get the list of changed/added files
+echo udf info: $(udf version)
+
+# get the list of changed/added CSV files
+git log | head
+git status
+cat $GITHUB_EVENT_PATH
+
 # TODO: for each file get the range of changed bars
 # TODO: for each file and range generate the patch
 # TODO: for each file and range upload patch to S3 bucket
