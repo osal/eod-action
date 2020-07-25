9068b34e753bac25ed9dc6e652180c045c9573fc
Aleksey Osipov <aosipov@tradingview.com>
2020-07-25 14:50:01 +0300

    print changed lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh -l
 
-set -x
-
 echo $(git version)
 echo $(gh --version)
 echo $(aws --version)
 echo $(jq --version)
-
-env
-ls -lR
 
 # download udf utility
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_UDF}
@@ -21,10 +16,21 @@ echo udf info: $(udf version)
 # get the list of changed/added CSV files
 PREVIOUS_HASH=$(cat $GITHUB_EVENT_PATH | jq .before | sed 's/"//g')
 git pull --unshallow
-CHANGED_DATA_FILES=$(git diff --name-only HEAD..$PREVIOUS_HASH | grep csv)
-echo $CHANGED_DATA_FILES
-# TODO: for each file get the range of changed bars
-# TODO: for each file and range generate the patch
-# TODO: for each file and range upload patch to S3 bucket
-# TODO: for each file and range create a task definition
-# TODO: send created tasks to SQS
+CHANGED_DATA_FILES=$(git diff --name-only --diff-filter=AM $PREVIOUS_HASH..HEAD | grep csv)
+# for each file get the range of changed bars
+for F in $CHANGED_DATA_FILES; do
+    echo processing file $F
+    FIRST_LINE=$(git diff --unified=0  $PREVIOUS_HASH..HEAD $F | grep @@ | head -n1 | cut -f2 -d" "| cut -c2-)
+    PATCH_FILE=$F.patch
+    tail -n +$FIRST_LINE $F > $PATCH_FILE
+    echo patch for $F
+    cat $PATCH_FILE
+    # TODO: generate the patch
+    # TODO: upload patch to S3 bucket
+    # TODO: create a task definition
+    # TODO: send created tasks to SQS
+done
+
+
+
+
